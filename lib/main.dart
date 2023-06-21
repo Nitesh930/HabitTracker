eab0ef1976236d7fc459bd5b9d514fca3f1ed356
NITESH  KUMAR SINGH <singhniteshkumar1234@gmail.com>
2023-06-21 20:13:35 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:task/profile.dart';
+import 'package:task/progress.dart';
+
+import 'HomePage.dart';
+import 'habitPage.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NavigationScreen(
+        currentIndex: 3,
+      ),
+    );
+  }
+}
+
+class NavigationScreen extends StatefulWidget {
+  NavigationScreen({required this.currentIndex});
+  int currentIndex;
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+const List<Widget> screens = [
+  HomePage(),
+  ProgressPage(),
+  Habitspage(),
+  ProfilePage(),
+];
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: widget.currentIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white24,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.blueAccent.shade400,
+        currentIndex: widget.currentIndex,
+        onTap: (index) {
+          setState(() {
+            widget.currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today), label: "Progress"),
+          BottomNavigationBarItem(icon: Icon(Icons.timeline), label: "Habits"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
+        ],
+      ),
+    );
+  }
+}
